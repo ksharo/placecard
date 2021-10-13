36541de98755cf7e62a50babeb7e428602ddfcbd
ksharo <ksharo@stevens.edu>
2021-10-13 16:34:24 -0400

    added basic html/css for header component

---
+++ placecard/src/components/shared/header.tsx
@@ -0,0 +1,17 @@
+import logo from '../../assets/logo.svg';
+import './headFoot.css';
+
+export function Header() {
+    return (
+    <header className='header-footer'>
+        <img className='logo' src={logo} alt='Logo'></img>
+        <section id='login'>
+          <section className='loginUserInput'>
+            <input className="borderless" type='text' id='username' placeholder='Username'></input>
+            <input className="borderless" type='password' id='password' placeholder='Password'></input>
+          </section>
+          <button className='rectangleButton'>Log In</button>
+        </section>
+      </header>
+    );
+}
