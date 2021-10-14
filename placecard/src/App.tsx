b79f91dee123b88907f7aa7138a062c662a0dc96
ksharo <ksharo@stevens.edu>
2021-10-14 19:34:23 -0400

    added basic html and styling for newAccount component

--- placecard/src/App.tsx
+++ placecard/src/App.tsx
@@ -3,19 +3,25 @@ import { Home } from './components/home/Home';
 import { Footer } from './components/shared/Footer';
 import { Header } from './components/shared/Header';
 import {
-  BrowserRouter as Router
+  BrowserRouter as Router, 
+  Route, 
+  Switch,
 } from "react-router-dom";
+import { NewAccount } from './components/forms/NewAccount';
 
 function App() {
   document.title = 'Placecard';
   return (
-    <body>
-      <Header></Header>
-      <Router>
-      <Home></Home>
-      </Router>
-      <Footer></Footer>
-    </body>
+    <Router>
+      <body>
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={ Home }/>
+          <Route exact path='/newAccount' component={ NewAccount }/>
+        </Switch>
+        <Footer/>
+      </body>
+    </Router>
   );
 }
 
---
+++ placecard/src/components/forms/NewAccount.tsx
@@ -0,0 +1,36 @@
+import './NewAccount.css'
+import {
+    Link
+  } from "react-router-dom";
+
+
+export function NewAccount() {
+    return (
+    <>
+        <h1>Welcome to Placecard!</h1>
+        <p>Let's get started with some basic information:</p>
+        
+        <form className='vertical-form'>
+            <label>First Name
+            <input type="text"/>
+            </label>
+            <label>Last Name
+            <input type="text"/>
+            </label>
+            <label>Phone Number (optional)
+            <input type="text"/>
+            </label>
+            <label>Email
+            <input type="text"/>
+            </label>
+            <label>Password
+            <input type="text"/>
+            </label>
+            <label>Confirm Password
+            <input type="text"/>
+            </label>
+        </form>
+        <Link to='/createEvent' className='rectangleButton smallerButton'>Next</Link>
+    </>
+    );
+}
