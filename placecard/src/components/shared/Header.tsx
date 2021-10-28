7a968645da676424d8eae3e91ec477f29fe90b96
ksharo <ksharo@stevens.edu>
2021-10-28 13:22:17 -0400

    added basic code for dynamic header

--- placecard/src/components/shared/Header.tsx
+++ placecard/src/components/shared/Header.tsx
@@ -1,23 +1,30 @@
 import { useHistory } from "react-router-dom";
 import logo from '../../assets/logo.png';
 import './HeadFoot.css';
+import { ProfileMenu } from './ProfileMenu';
 
 export function Header() {
   const history = useHistory();
+  let loggedIn = true;
 
   let handleClick =  () => {
     history.push('/');
   }   
-    return (
-    <header className='header-footer'>
-        <img className='logo' onClick={handleClick} src={logo} alt='Logo'></img>
+  let profile = <ProfileMenu name='Apple Zebra' id='1'/>;
+  
+  let login = 
         <section id='login'>
           <section className='loginUserInput'>
             <input className="borderless" type='text' id='username' placeholder='Username'></input>
             <input className="borderless" type='password' id='password' placeholder='Password'></input>
           </section>
           <button className='rectangleButton'>Log In</button>
-        </section>
+        </section>;
+
+    return (
+    <header className='header-footer'>
+        <img className='logo' onClick={handleClick} src={logo} alt='Logo'></img>
+           { loggedIn ? profile : login}
       </header>
     );
 }
---
+++ placecard/src/components/shared/ProfileMenu.tsx
@@ -0,0 +1,38 @@
+import './HeadFoot.css';
+import Avatar from '@mui/material/Avatar';
+import React from 'react';
+import { IconButton, Menu, MenuItem } from '@mui/material';
+
+export function ProfileMenu(props: {name: string; id: string}) {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: any) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
+    const names = props.name.split(' ');
+    let initials = '';
+    if (names.length > 1) {
+        initials = names[0][0] + names[names.length-1][0];
+    }
+    else {
+        initials = names[0][0];
+    }
+
+    return (
+        <>
+            <Avatar className='profilePic' onClick={ handleClick } sx={{ width: 46, height: 46 }}>{initials}</Avatar>
+            <Menu anchorEl={ anchorEl } 
+                open={ open } 
+                onClose ={ handleClose } 
+                onClick={ handleClose }>
+                <MenuItem id='userName' style={{ pointerEvents: 'none' }}>{props.name}</MenuItem>
+                <MenuItem>View/Edit Profile</MenuItem>
+                <MenuItem>Logout</MenuItem>
+            </Menu>
+        </>
+    );
+}
