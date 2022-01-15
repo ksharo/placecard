3b03f7c55d6d8a25c056ed28faad207115fd6cc3
Jayson Infante <jayson.infante708@gmail.com>
2022-01-15 13:49:41 -0500

    Updated mongo connection code to reflect some of hill's updates last sem

--- placecardAPI/config/mongoConfig/mongoCollections.js
+++ placecardAPI/config/mongoConfig/mongoCollections.js
@@ -1,19 +1,17 @@
-const dbConnection = require('./mongoConnection');
-const { isUndefined } = require('lodash');
+const dbConnection = require("./mongoConnection");
+const { isUndefined } = require("lodash");
 
 const getCollection = (collection) => {
-    let col = undefined;
-    return async() => {
-        if (isUndefined(col)) {
-            console.log('about to await')
-            const db = await dbConnection();
-            console.log(db)
-            col = await db.collection(collection);
-        }
-        return col;
-    };
+  let col = undefined;
+  return async () => {
+    if (isUndefined(col)) {
+      const db = await dbConnection.connectToDb();
+      col = await db.collection(collection);
+    }
+    return col;
+  };
 };
 
 module.exports = {
-    events: getCollection('events')
+  events: getCollection("events"),
 };
--- placecardAPI/config/mongoConfig/mongoConnection.js
+++ placecardAPI/config/mongoConfig/mongoConnection.js
@@ -1,18 +1,22 @@
-const { MongoClient } = require('mongodb');
-const { isUndefined } = require('lodash');
-const { mongoConfig } = require('./mongoSettings');
+const { MongoClient } = require("mongodb");
+const { isUndefined } = require("lodash");
+const { mongoConfig } = require("./mongoSettings");
 
 let connection = undefined;
 let db = undefined;
 
-module.exports = async() => {
+module.exports = {
+  connectToDb: async () => {
     if (isUndefined(connection)) {
-        console.log('db1')
-        console.log(mongoConfig.dbUrl);
-        connection = await MongoClient.connect(mongoConfig.dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-        console.log('db2')
-        db = await connection.db(mongoConfig.database);
-        console.log('db3')
+      connection = await MongoClient.connect(mongoConfig.dbUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: false,
+      });
+      db = await connection.db(mongoConfig.database);
     }
     return db;
-}
+  },
+  closeConnection: () => {
+    connection.close();
+  },
+};
