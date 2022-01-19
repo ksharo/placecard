0f582fe0c08bd20b7390fdebb6362d4cddaadd22
Jayson Infante <jayson.infante708@gmail.com>
2022-01-19 16:22:17 -0500

    Created GET route for guests

---
+++ placecardAPI/src/routes/guestRoutes.js
@@ -0,0 +1,47 @@
+const _ = require("lodash");
+const express = require("express");
+const router = express.Router();
+const { guests } = require("../data");
+const {
+  INVALID_GUEST_ID_MESSAGE,
+  GUEST_UNDEFINED_MESSAGE,
+  GUEST_EMPTY_MESSAGE,
+} = require("../constants/errorMessages");
+const {
+  generateErrorMessage,
+  createErrorResponse,
+} = require("../utils/errors");
+const { SCHEMA_TYPES } = require("../constants/schemaTypes");
+const { validateSchema } = require("../utils/schemaValidator");
+const { checkPrecondition } = require("../utils/preconditions");
+const statusCodes = require("../constants/statusCodes");
+const ERROR_TYPES = require("../constants/errorTypes");
+const { isInvalidObjectId } = require("../utils/mongoDocument");
+const { INVALID_GUEST_ID } = require("../constants/errorTypes");
+
+router.get("/:guestId", async (req, res) => {
+  let guestId = req.params.guestId.trim();
+  try {
+    checkPrecondition(guestId, _.isUndefined, INVALID_GUEST_ID_MESSAGE);
+    checkPrecondition(guestId, isInvalidObjectId, INVALID_GUEST_ID_MESSAGE);
+  } catch (e) {
+    return createErrorResponse(
+      e.message,
+      ERROR_TYPES.INVALID_GUEST_ID,
+      statusCodes.BAD_REQUEST,
+      res
+    );
+  }
+
+  try {
+    const guest = await guests.getGuest(guestId);
+    return res.json(guest);
+  } catch (e) {
+    return createErrorResponse(
+      generateErrorMessage(e),
+      ERROR_TYPES.GUEST_NOT_FOUND,
+      statusCodes.NOT_FOUND,
+      res
+    );
+  }
+});
