b8453ec6564a2f21436cf4432e439195aef94793
ksharo <ksharo@stevens.edu>
2022-01-22 17:26:14 -0500

    started drag and drop on last survey page

--- placecardClient/src/components/guestPages/EditSurveyResponses.tsx
+++ placecardClient/src/components/guestPages/EditSurveyResponses.tsx
@@ -1,102 +1,153 @@
 import React, { useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-import uuid from "uuid/v4";
+import {uuid} from "uuidv4";
 
 export function EditSurveyResponses() {
 
-    const columns = {
+    const unusedInvitees = [];
+    for (let x of window.inviteesState) {
+        if (!(window.lovedInvitees.includes(x) || window.likedInvitees.includes(x) || window.dislikedInvitees.includes(x))) {
+            unusedInvitees.push(x);
+        }
+    }
+    const disliked = [...window.dislikedInvitees];
+    const liked = [...window.likedInvitees];
+    const loved = [...window.lovedInvitees];
+    const origColumns = {
         [uuid()]: {
             name: 'Avoid Sitting With',
-            items: window.dislikedInvitees
+            items: disliked
         },
         [uuid()]: {
             name: 'Comfortable Sitting With',
-            items: window.likedInvitees
+            items: liked
         },
         [uuid()]: {
             name: 'Ideal Table',
-            items: window.lovedInvitees
+            items: loved
         },
         [uuid()]: {
             name: 'Others',
-            items: window.inviteesState
+            items: unusedInvitees
+        }
+    }
+
+    const [columns, setColumns] = React.useState(origColumns);
+
+    const onDragEnd = (result: any, columns: any, setColumns: any) => {
+        if (!result.destination) return;
+        const { source, destination } = result;
+        if (source.droppableId !== destination.droppableId) {
+            const sourceColumn = columns[source.droppableId];
+            const destColumn = columns[destination.droppableId];
+            const sourceItems = [...sourceColumn.items];
+            const destItems = [...destColumn.items];
+            const [removed] = sourceItems.splice(source.index, 1);
+            destItems.splice(destination.index, 0, removed);
+            setColumns({
+            ...columns,
+            [source.droppableId]: {
+                ...sourceColumn,
+                items: sourceItems
+            },
+            [destination.droppableId]: {
+                ...destColumn,
+                items: destItems
+            }
+            });
+        } else {
+            const column = columns[source.droppableId];
+            const copiedItems = [...column.items];
+            const [removed] = copiedItems.splice(source.index, 1);
+            copiedItems.splice(destination.index, 0, removed);
+            setColumns({
+            ...columns,
+            [source.droppableId]: {
+                ...column,
+                items: copiedItems
+            }
+            });
         }
     }
 
       return (
+        <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
         <DragDropContext
-          onDragEnd={onDragEnd}
+            onDragEnd={result => onDragEnd(result, columns, setColumns)}
         >
-          <div>Hello world</div>
+            {Object.entries(columns).map(([columnId, column], index) => {
+            return (
+                <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center"
+                }}
+                key={columnId}
+                >
+                <h2>{column.name}</h2>
+                <div style={{ margin: 8 }}>
+                    <Droppable droppableId={columnId} key={columnId}>
+                    {(provided, snapshot) => {
+                        return (
+                        <div
+                            {...provided.droppableProps}
+                            ref={provided.innerRef}
+                            style={{
+                            background: snapshot.isDraggingOver
+                                ? "lightblue"
+                                : "lightgrey",
+                            padding: 4,
+                            width: 250,
+                            minHeight: 500
+                            }}
+                        >
+                            {column.items.map((item, index) => {
+                            return (
+                                <Draggable
+                                key={item.id}
+                                draggableId={item.id}
+                                index={index}
+                                >
+                                {(provided, snapshot) => {
+                                    return (
+                                    <div
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}
+                                        style={{
+                                        userSelect: "none",
+                                        padding: 16,
+                                        margin: "0 0 8px 0",
+                                        minHeight: "50px",
+                                        backgroundColor: snapshot.isDragging
+                                            ? "#263B4A"
+                                            : "#456C86",
+                                        color: "white",
+                                        ...provided.draggableProps.style
+                                        }}
+                                    >
+                                        {item.name}
+                                    </div>
+                                    );
+                                }}
+                                </Draggable>
+                            );
+                            })}
+                            {provided.placeholder}
+                        </div>
+                        );
+                    }}
+                    </Droppable>
+                </div>
+                </div>
+            );
+            })}
         </DragDropContext>
+        </div>
       );
 }
 
-
-
-// const itemsFromBackend = [
-//   { id: uuid(), content: "First task" },
-//   { id: uuid(), content: "Second task" },
-//   { id: uuid(), content: "Third task" },
-//   { id: uuid(), content: "Fourth task" },
-//   { id: uuid(), content: "Fifth task" }
-// ];
-
-// const columnsFromBackend = {
-//   [uuid()]: {
-//     name: "Requested",
-//     items: itemsFromBackend
-//   },
-//   [uuid()]: {
-//     name: "To do",
-//     items: []
-//   },
-//   [uuid()]: {
-//     name: "In Progress",
-//     items: []
-//   },
-//   [uuid()]: {
-//     name: "Done",
-//     items: []
-//   }
-// };
-
-// const onDragEnd = (result, columns, setColumns) => {
-//   if (!result.destination) return;
-//   const { source, destination } = result;
-
-//   if (source.droppableId !== destination.droppableId) {
-//     const sourceColumn = columns[source.droppableId];
-//     const destColumn = columns[destination.droppableId];
-//     const sourceItems = [...sourceColumn.items];
-//     const destItems = [...destColumn.items];
-//     const [removed] = sourceItems.splice(source.index, 1);
-//     destItems.splice(destination.index, 0, removed);
-//     setColumns({
-//       ...columns,
-//       [source.droppableId]: {
-//         ...sourceColumn,
-//         items: sourceItems
-//       },
-//       [destination.droppableId]: {
-//         ...destColumn,
-//         items: destItems
-//       }
-//     });
-//   } else {
-//     const column = columns[source.droppableId];
-//     const copiedItems = [...column.items];
-//     const [removed] = copiedItems.splice(source.index, 1);
-//     copiedItems.splice(destination.index, 0, removed);
-//     setColumns({
-//       ...columns,
-//       [source.droppableId]: {
-//         ...column,
-//         items: copiedItems
-//       }
-//     });
-//   }
-// };
 
 // function App() {
 //   const [columns, setColumns] = useState(columnsFromBackend);
@@ -179,3 +230,72 @@ export function EditSurveyResponses() {
 // }
 
 // export default App;
+
+
+//// DONEEEEEEEEEEE
+
+// const itemsFromBackend = [
+//   { id: uuid(), content: "First task" },
+//   { id: uuid(), content: "Second task" },
+//   { id: uuid(), content: "Third task" },
+//   { id: uuid(), content: "Fourth task" },
+//   { id: uuid(), content: "Fifth task" }
+// ];
+
+// const columnsFromBackend = {
+//   [uuid()]: {
+//     name: "Requested",
+//     items: itemsFromBackend
+//   },
+//   [uuid()]: {
+//     name: "To do",
+//     items: []
+//   },
+//   [uuid()]: {
+//     name: "In Progress",
+//     items: []
+//   },
+//   [uuid()]: {
+//     name: "Done",
+//     items: []
+//   }
+// };
+
+
+
+// const onDragEnd = (result, columns, setColumns) => {
+//   if (!result.destination) return;
+//   const { source, destination } = result;
+
+//   if (source.droppableId !== destination.droppableId) {
+//     const sourceColumn = columns[source.droppableId];
+//     const destColumn = columns[destination.droppableId];
+//     const sourceItems = [...sourceColumn.items];
+//     const destItems = [...destColumn.items];
+//     const [removed] = sourceItems.splice(source.index, 1);
+//     destItems.splice(destination.index, 0, removed);
+//     setColumns({
+//       ...columns,
+//       [source.droppableId]: {
+//         ...sourceColumn,
+//         items: sourceItems
+//       },
+//       [destination.droppableId]: {
+//         ...destColumn,
+//         items: destItems
+//       }
+//     });
+//   } else {
+//     const column = columns[source.droppableId];
+//     const copiedItems = [...column.items];
+//     const [removed] = copiedItems.splice(source.index, 1);
+//     copiedItems.splice(destination.index, 0, removed);
+//     setColumns({
+//       ...columns,
+//       [source.droppableId]: {
+//         ...column,
+//         items: copiedItems
+//       }
+//     });
+//   }
+// };
