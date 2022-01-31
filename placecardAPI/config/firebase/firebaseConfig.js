5d27448e798bb643ed1ec3b10a2bb3844ae7e80c
Simon Gao <sgao1202@gmail.com>
2022-01-31 00:36:01 -0500

    Added basic Firebase structure to backend

---
+++ placecardAPI/config/firebase/firebaseConfig.js
@@ -0,0 +1,7 @@
+const firebaseConfig = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PROJECT_PRIVATE_KEY,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL
+};
+
+module.exports = Object.freeze(firebaseConfig);
