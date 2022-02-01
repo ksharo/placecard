bdb920856bb9ea20ff82f12450d9efe359515430
Jayson Infante <jayson.infante708@gmail.com>
2022-02-01 18:04:49 -0500

    Fixed syntax error in survey response schema

--- placecardAPI/src/schema/Guest.js
+++ placecardAPI/src/schema/Guest.js
@@ -2,40 +2,23 @@ const Joi = require("joi");
 
 // TODO: Add properties to surveyResponseSchema, or remove it later on
 const surveyResponseSchema = Joi.object({
-    disliked: Joi.array().
-        required(),
-    liked: Joi.array().
-        required(),
-    ideal: Joi.array(),
-        required()
+    disliked: Joi.array().required(),
+    liked: Joi.array().required(),
+    ideal: Joi.array().required(),
 });
 
 const schema = Joi.object({
     _id: Joi.string(),
-    first_name: Joi.string().
-        min(2).
-        required(),
-    last_name: Joi.string().
-        min(2).
-        required(),
-    email: Joi.string().
-        email().
-        required(),
-    phone_number: Joi.string().
-        required(),
-    age: Joi.number().
-        integer().
-        required(),
+    first_name: Joi.string().min(2).required(),
+    last_name: Joi.string().min(2).required(),
+    email: Joi.string().email().required(),
+    phone_number: Joi.string().required(),
+    age: Joi.number().integer().required(),
     is_locked: Joi.boolean(),
-    party_size: Joi.number().
-        integer().
-        required(),
-    associated_table_number: Joi.number().
-        integer(),
-    children: Joi.array().
-        required(),
-    survey_response: surveyResponseSchema.
-        required()
+    party_size: Joi.number().integer().required(),
+    associated_table_number: Joi.number().integer(),
+    children: Joi.array().required(),
+    survey_response: surveyResponseSchema.required(),
 }).strict();
 
 module.exports = schema;
