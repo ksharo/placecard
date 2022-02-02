1f9d1eaff3fe68986ca13d114895db04b4e9dddd
Ookayleb <calebonthemove@gmail.com>
2022-02-02 17:31:31 -0500

    geust row

--- placecardClient/src/components/guestList/GuestListRow.tsx
+++ placecardClient/src/components/guestList/GuestListRow.tsx
@@ -31,12 +31,39 @@ export function GuestListRow({ rowNum, guestName, email, phone, partySize, isVip
 				<input type='phone'		value={rowState.phone}		onChange={(e) => {setRowState(prevState => ({...rowState, phone: e.target.value}))}}/>
 				<input type='number'	value={rowState.partySize}	onChange={(e) => {setRowState(prevState => ({...rowState, partySize: parseInt(e.target.value)}))}}/>
 				<input type='checkbox'	checked={rowState.isVip}		onChange={(e) => {setRowState(prevState => ({...rowState, isVip: e.target.checked}))}}/> */}
-				<TextField name={"name" + rowNum} inputProps={{autoComplete: "disabled"}} className= 'nameColumn'/>
-				<TextField name={"email" + rowNum} type="email" inputProps={{autoComplete: "disabled"}}/>
-				<TextField name={"phone" + rowNum} type="phone" inputProps={{autoComplete: "disabled"}}/>
-				<TextField name={"partySize" + rowNum} type="number" inputProps={{autoComplete: "disabled"}}/>
-				<Checkbox name={"isVip" + rowNum}/>
-				<button tabIndex={-1} id={"deleteRow" + rowNum} onClick={ () => deleteRowFunction(rowNum) } type="button"><HiTrash/></button>
+				<TextField
+					name={"name" + rowNum}
+					value={rowState.guestName}
+					inputProps={{autoComplete: "disabled"}}
+					className= 'nameColumn'
+					onBlur={(event)=>  setRowState( (prev) => ({ ...prev, guestName: event.target.value }) )}
+				/>
+				<TextField
+					name={"email" + rowNum}
+					type="email"
+					inputProps={{autoComplete: "disabled"}}
+				/>
+				<TextField
+					name={"phone" + rowNum}
+					type="phone"
+					inputProps={{autoComplete: "disabled"}}
+				/>
+				<TextField
+					name={"partySize" + rowNum}
+					type="number"
+					inputProps={{autoComplete: "disabled"}}
+				/>
+				<Checkbox
+					name={"isVip" + rowNum}
+				/>
+				<button
+					tabIndex={-1}
+					id={"deleteRow" + rowNum}
+					onClick={ () => deleteRowFunction(rowNum) }
+					type="button">
+
+					<HiTrash/>
+				</button>
 
 				{/* <button><IoTrashBinSharp/></button> */}
 			{/* </form> */}
