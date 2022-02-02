20d47dee6dbe9c69ef475aff14b2438d13b37652
Jayson Infante <jayson.infante708@gmail.com>
2022-02-01 22:38:17 -0500

    Uncommented guests route

--- placecardAPI/src/routes/index.js
+++ placecardAPI/src/routes/index.js
@@ -1,17 +1,17 @@
 const eventsRoutes = require("./eventRoutes");
 const guestRoutes = require("./guestRoutes");
 const userRoutes = require("./userRoutes");
-var cors = require('cors');
+var cors = require("cors");
 
 // TODO: make cors only for our apps
 const constructorMethod = (app) => {
-    app.options('/events', cors());
-    app.use('/events', cors(), eventsRoutes);
+    app.options("/events", cors());
+    app.use("/events", cors(), eventsRoutes);
 
     app.use("/events", eventsRoutes);
-    // app.use("/guests", guestRoutes);
-    
-    app.use('*', (req, res) => {
+    app.use("/guests", guestRoutes);
+
+    app.use("*", (req, res) => {
         res.sendStatus(404);
     });
 };
