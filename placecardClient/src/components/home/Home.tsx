e8d86cdc02e7c6c51a457310424c55ed0e52e50b
ksharo <ksharo@stevens.edu>
2022-02-20 14:38:45 -0500

    started styling home page

--- placecardClient/src/components/home/Home.tsx
+++ placecardClient/src/components/home/Home.tsx
@@ -1,5 +1,9 @@
 import { Button } from '@mui/material';
 import mainImg from '../../assets/placeholderImage.jpeg';
+import {BsFillCloudArrowUpFill} from 'react-icons/bs'
+import {RiSurveyFill} from 'react-icons/ri'
+import {FaUmbrellaBeach} from 'react-icons/fa'
+
 import './Home.css';
 
 export function Home() {
@@ -12,7 +16,42 @@ export function Home() {
                         <h4>A modernly versatile seating-chart application</h4>
                         <Button variant='contained' color='info'>Get Started!</Button>
                     </section>
-                    <img id='mainImage' src={mainImg}/>
+                </section>
+                <img id='mainImage' src={mainImg}/>
+            </section>
+            <section className='mainContent'>
+                <h1 className='title'>How It Works:</h1>
+                <section className='howItWorksInstructions'>
+                    <section className='instructionStep'>
+                        <section className='stepNumber'>
+                            1
+                        </section>
+                        <section className='stepCard'>
+                            <BsFillCloudArrowUpFill/>
+                            <h2>Upload Guest List</h2>
+                            <p>Enter some basic information about your event and share your guest list with us to get started.</p>
+                        </section>
+                    </section>
+                    <section className='instructionStep'>
+                        <section className='stepNumber'>
+                            2
+                        </section>
+                        <section className='stepCard'>
+                            <RiSurveyFill/>
+                            <h2>Send Survey</h2>
+                            <p>Customize a message inviting your guests to fill out our seating survey and send it to whoever you want!</p>
+                        </section>
+                    </section>
+                    <section className='instructionStep'>
+                        <section className='stepNumber'>
+                            3
+                        </section>
+                        <section className='stepCard'>
+                            <FaUmbrellaBeach/>
+                            <h2>Placecard Magic!</h2>
+                            <p>Sit back, relax, and let Placecard do the rest of the work! We will seat guests based on their responses, but feel free to edit on your own as well!</p>
+                        </section>
+                    </section>
                 </section>
             </section>
         </>
