20d73166ad73d25afaac0eceabb7f1e51e9c328a
Jayson Infante <jayson.infante708@gmail.com>
2022-03-04 13:22:11 -0500

    Adjusted event schema again

--- placecardAPI/src/schema/Event.js
+++ placecardAPI/src/schema/Event.js
@@ -2,27 +2,23 @@ const Joi = require("joi");
 
 const schema = Joi.object({
     _id: Joi.string(),
-    event_name: Joi.string().
-        required(),
+    event_name: Joi.string().required(),
     tables: Joi.array(),
-    event_start_time: Joi.number().
-        integer().
-        required(),
-    event_end_time: Joi.number().
-        integer().
-        greater(Joi.ref("event_start_time")),
+    event_start_time: Joi.number().integer().required(),
+    // event_end_time: Joi.number().
+    //     integer().
+    //     greater(Joi.ref("event_start_time")),
     location: Joi.string(),
-    expected_number_of_attendees: Joi.number().
-        integer().
-        greater(0).
-        required(),
-    attendees_per_table: Joi.number().
-        integer().
-        greater(0).
-        max(Joi.ref("expected_number_of_attendees")).
-        required(),
-    guest_list: Joi.array().
-        required()
+    // expected_number_of_attendees: Joi.number().
+    //     integer().
+    //     greater(0).
+    //     required(),
+    attendees_per_table: Joi.number()
+        .integer()
+        .greater(0)
+        .max(Joi.ref("expected_number_of_attendees"))
+        .required(),
+    guest_list: Joi.array().required(),
 }).strict();
 
 module.exports = schema;
