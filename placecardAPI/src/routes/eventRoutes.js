e4e9b7d7a420938971e6b9596c2bb3259a89613a
Jayson Infante <jayson.infante708@gmail.com>
2022-03-04 14:21:55 -0500

    Removed print statements

--- placecardAPI/src/routes/eventRoutes.js
+++ placecardAPI/src/routes/eventRoutes.js
@@ -2,8 +2,15 @@ const _ = require("lodash");
 const express = require("express");
 const router = express.Router();
 const { events } = require("../data");
-const { INVALID_EVENT_ID_MESSAGE, EVENT_UNDEFINED_MESSAGE, EVENT_EMPTY_MESSAGE } = require("../constants/errorMessages");
-const { generateErrorMessage, createErrorResponse } = require("../utils/errors");
+const {
+    INVALID_EVENT_ID_MESSAGE,
+    EVENT_UNDEFINED_MESSAGE,
+    EVENT_EMPTY_MESSAGE,
+} = require("../constants/errorMessages");
+const {
+    generateErrorMessage,
+    createErrorResponse,
+} = require("../utils/errors");
 const { SCHEMA_TYPES } = require("../constants/schemaTypes");
 const { validateSchema } = require("../utils/preconditions");
 const { checkPrecondition } = require("../utils/preconditions");
@@ -15,18 +22,28 @@ const { ObjectId } = require("mongodb");
 
 router.get("/:eventId", async (req, res) => {
     let eventId = req.params.eventId.trim();
-    try { 
-        checkPrecondition(eventId, _.isUndefined, INVALID_EVENT_ID_MESSAGE);    
+    try {
+        checkPrecondition(eventId, _.isUndefined, INVALID_EVENT_ID_MESSAGE);
         checkPrecondition(eventId, isInvalidObjectId, INVALID_EVENT_ID_MESSAGE);
     } catch (e) {
-        return createErrorResponse(e.message, ERROR_TYPES.INVALID_EVENT_ID, statusCodes.BAD_REQUEST, res);
+        return createErrorResponse(
+            e.message,
+            ERROR_TYPES.INVALID_EVENT_ID,
+            statusCodes.BAD_REQUEST,
+            res
+        );
     }
 
     try {
         const event = await events.getEvent(eventId);
         return res.json(event);
     } catch (e) {
-        return createErrorResponse(generateErrorMessage(e), ERROR_TYPES.EVENT_NOT_FOUND, statusCodes.NOT_FOUND, res);
+        return createErrorResponse(
+            generateErrorMessage(e),
+            ERROR_TYPES.EVENT_NOT_FOUND,
+            statusCodes.NOT_FOUND,
+            res
+        );
     }
 });
 
@@ -37,14 +54,24 @@ router.post("/newEvent", async (req, res) => {
         checkPrecondition(newEvent, _.isEmpty, EVENT_EMPTY_MESSAGE);
         validateSchema(newEvent, SCHEMA_TYPES.EVENT);
     } catch (e) {
-        return createErrorResponse(e.message, ERROR_TYPES.INVALID_EVENT, statusCodes.BAD_REQUEST, res);
+        return createErrorResponse(
+            e.message,
+            ERROR_TYPES.INVALID_EVENT,
+            statusCodes.BAD_REQUEST,
+            res
+        );
     }
     try {
         const createdEvent = await events.createEvent(newEvent);
         return res.json(createdEvent);
-    } catch(e) {        
-        return createErrorResponse(e.message, ERROR_TYPES.INSERT_ERROR, statusCodes.INTERNAL_SERVER, res);
-    } 
+    } catch (e) {
+        return createErrorResponse(
+            e.message,
+            ERROR_TYPES.INSERT_ERROR,
+            statusCodes.INTERNAL_SERVER,
+            res
+        );
+    }
 });
 
 router.put("/updateEvent/:id", async (req, res) => {
@@ -55,28 +82,37 @@ router.put("/updateEvent/:id", async (req, res) => {
         checkPrecondition(updatedEvent, _.isEmpty, EVENT_EMPTY_MESSAGE);
         validateSchema(updatedEvent, SCHEMA_TYPES.EVENT);
     } catch (e) {
-        return createErrorResponse(e.message, ERROR_TYPES.INVALID_EVENT, statusCodes.BAD_REQUEST, res);
+        return createErrorResponse(
+            e.message,
+            ERROR_TYPES.INVALID_EVENT,
+            statusCodes.BAD_REQUEST,
+            res
+        );
     }
 
     const eventId = req.params.id;
     try {
-        console.log('hi');
         checkPrecondition(eventId, _.isUndefined, INVALID_EVENT_ID);
-        console.log('hi');
         checkPrecondition(eventId, isInvalidObjectId, INVALID_EVENT_ID);
-        console.log('hi');
-    } catch(e) {
-        return createErrorResponse(e.message, ERROR_TYPES.INVALID_EVENT_ID, INVALID_EVENT_ID, res)
+    } catch (e) {
+        return createErrorResponse(
+            e.message,
+            ERROR_TYPES.INVALID_EVENT_ID,
+            INVALID_EVENT_ID,
+            res
+        );
     }
 
     try {
-        console.log('hi');
         const event = await events.updateEvent(eventId, updatedEvent);
-        console.log('hi');
         return res.json(event);
     } catch (e) {
-        console.log(e);
-        return createErrorResponse(e.message, ERROR_TYPES.UPDATE_ERROR, statusCodes.INTERNAL_SERVER, res);
+        return createErrorResponse(
+            e.message,
+            ERROR_TYPES.UPDATE_ERROR,
+            statusCodes.INTERNAL_SERVER,
+            res
+        );
     }
 });
 
@@ -85,23 +121,38 @@ router.delete("/:eventId", async (req, res) => {
     try {
         checkPrecondition(eventId, _.isUndefined, INVALID_EVENT_ID_MESSAGE);
     } catch (e) {
-        return createErrorResponse(e.message, ERROR_TYPES.INVALID_EVENT_ID, statusCodes.BAD_REQUEST, res);
+        return createErrorResponse(
+            e.message,
+            ERROR_TYPES.INVALID_EVENT_ID,
+            statusCodes.BAD_REQUEST,
+            res
+        );
     }
 
     try {
         const event = await events.getEvent(eventId);
     } catch (e) {
-        return createErrorResponse(e.message, ERROR_TYPES.EVENT_NOT_FOUND, statusCodes.NOT_FOUND, res);
+        return createErrorResponse(
+            e.message,
+            ERROR_TYPES.EVENT_NOT_FOUND,
+            statusCodes.NOT_FOUND,
+            res
+        );
     }
 
     try {
         await events.deleteEvent(eventId);
         const response = {
-            snapshot_id: eventId
+            snapshot_id: eventId,
         };
         return res.json(response);
-    } catch(e) {
-        return createErrorResponse(e.message, ERROR_TYPES.EVENT_NOT_FOUND, statusCodes.INTERNAL_SERVER, res);
+    } catch (e) {
+        return createErrorResponse(
+            e.message,
+            ERROR_TYPES.EVENT_NOT_FOUND,
+            statusCodes.INTERNAL_SERVER,
+            res
+        );
     }
 });
 
