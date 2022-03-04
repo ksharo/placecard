12a0b7c394aa636e2f7133b87651139704ddf003
Gil Austria <gaustria@stevens.edu>
2022-03-04 16:33:51 -0500

    Generating random strings

--- placecardAPI/tasks/seed.js
+++ placecardAPI/tasks/seed.js
@@ -1,19 +1,61 @@
 const dbConnection = require("../config/mongoConfig/mongoConnection");
 const data = require("../src/data");
 const events = data.events;
-// const guests         = data.guests;
+const guests = data.guests;
+
+const { uniqueNamesGenerator, Config, countries, names, adjectives, colors, starWars, NumberDictionary } = require('unique-names-generator');
+
+const eventName = uniqueNamesGenerator({
+  dictionaries: [adjectives, starWars],
+  length: 2,
+  separator: " "
+});
+
+const tableNames = uniqueNamesGenerator({
+  dictionaries: [adjectives, colors],
+  length: 2,
+  separator: " "
+});
+
+function randomDate(start, end) {
+  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+}
+
+const location = uniqueNamesGenerator({
+  dictionaries: [countries]
+});
+
+const attendeesPerTable = NumberDictionary.generate({ min: 4, max: 16 });
+
+
+const guestNames = uniqueNamesGenerator({
+  dictionaries: [names]
+})
+
+const emailNumber = NumberDictionary.generate({min: 100, max: 999})
+const emails = uniqueNamesGenerator({
+  dictionaries: [adjectives, colors, emailNumber, ['@gmail.com']],
+  length: 4,
+  separator: "",
+  style: "capital"
+})
+
+
+
+// randomDate(new Date(2012, 0, 1), new Date())
+
+let eventSeed = [];
+let guestsSeed = [];
+
+
+function generateData() {
+
+}
 
 async function main() {
   const db = await dbConnection.connectToDb();
 
-  let walmartwedding = await events.createEvent({
-    event_name: "testEvent",
-    tables: [],
-    event_time: 300,
-    expected_number_of_attendees: 100,
-    attendees_per_table: 10,
-  });
-  console.log(walmartwedding._id);
+  
 
   console.log("Done seeding database");
 
