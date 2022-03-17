e3dfcb43f7ce65ee03d9649d1a6c6c5c57b23040
jinfante708 <jinfante@stevens.edu>
2022-03-16 22:16:18 -0500

    Created guest patch schema

---
+++ placecardAPI/src/schema/GuestPatch.js
@@ -0,0 +1,24 @@
+const Joi = require("joi");
+
+// TODO: Add properties to surveyResponseSchema, or remove it later on
+const surveyResponseSchema = Joi.object({
+    disliked: Joi.array().required(),
+    liked: Joi.array().required(),
+    ideal: Joi.array().required(),
+});
+
+const schema = Joi.object({
+    _id: Joi.string(),
+    first_name: Joi.string().min(2),
+    last_name: Joi.string().min(2),
+    // Only one is required between email/phone
+    email: Joi.string().email(),
+    phone_number: Joi.string(),
+    party_size: Joi.number().integer(),
+    associated_table_number: Joi.number().integer(),
+    group_id: Joi.string(),
+    group_name: Joi.string(),
+    survey_response: surveyResponseSchema,
+}).strict();
+
+module.exports = schema;
