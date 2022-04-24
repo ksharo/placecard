7f35cb47489ca42b68b91236eb16390b86a5f5c0
ksharo <ksharo@stevens.edu>
2022-04-23 22:37:44 -0400

    fixed divide by zero pie chart bug

--- placecardClient/src/components/dashboards/eventDash/EditCard.tsx
+++ placecardClient/src/components/dashboards/eventDash/EditCard.tsx
@@ -4,31 +4,34 @@ import { Button, CardActions, CardContent, CardHeader } from "@mui/material";
 import { PieChart } from 'react-minimal-pie-chart';
 
 
-export function EditCard(props: {numSent: string; numRec: string}) {
+export function EditCard(props: { numSent: string; numRec: string }) {
     const history = useHistory();
-    
+
     const toGuestListEdit = () => {
         history.push('/editGuestList');
     };
-    const percRec = cleanPercentage(parseInt(props.numRec)/parseInt(props.numSent));
+    const percRec = cleanPercentage(parseInt(props.numRec) / parseInt(props.numSent));
     return (
         <Card className='dashCard bigCard'>
-            <CardHeader title='Guest List' className='dashCardHeader'/>
+            <CardHeader title='Guest List' className='dashCardHeader' />
             <CardContent className='flexCard'>
                 <section>
                     <p><strong>Surveys Sent:</strong> {props.numSent} </p>
                     <p><strong>Surveys Completed:</strong> {props.numRec}/{props.numSent} </p>
                 </section>
-                <PieChart className='pieChart' totalValue={100} animate={true}
-                    data={[
-                        { title: 'Percent Completed', value: percRec, color: 'var(--highlight)' },
-                        { title: 'Percent Waiting', value: 100-percRec, color: '#e5e5e5' }
-                    ]}
+                {
+                    parseInt(props.numSent) > 0 && <PieChart className='pieChart' totalValue={100} animate={true}
+                        data={[
+                            { title: 'Percent Completed', value: percRec, color: 'var(--highlight)' },
+                            { title: 'Percent Waiting', value: 100 - percRec, color: '#e5e5e5' }
+                        ]}
                     />
+                }
+
             </CardContent>
             <CardActions className='cardFooter'>
                 <Button onClick={toGuestListEdit} className='basicBtn fitBtn' variant='contained'>
-                     Edit/View Guest List
+                    Edit/View Guest List
                 </Button>
             </CardActions>
         </Card>
