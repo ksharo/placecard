0142d38a72ea7d9c408de3e488491e8fcd67ef85
Simon Gao <sgao1202@gmail.com>
2022-04-28 17:00:03 -0400

    Delete console.logs from code

--- placecardAPI/src/constants/configs.js
+++ placecardAPI/src/constants/configs.js
@@ -1,6 +1,5 @@
 const { EMAIL_TEMPLATE_SUBJECT  } = require("./messages");
 
-console.log("Placecard email: ", process.env.PLACECARD_EMAIL);
 const baseEmailConfig = {
     from: process.env.PLACECARD_EMAIL,
     subject: EMAIL_TEMPLATE_SUBJECT,
