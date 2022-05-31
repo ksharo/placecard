261174511c7f7162fba5dee383573dc131168450
ksharo <ksharo@stevens.edu>
2022-05-31 11:29:54 -0400

    took out server code for deployment testing

--- startScript.py
+++ startScript.py
@@ -1,7 +1,7 @@
 import os
 # import pip3
-os.system("curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py")
-os.system("python3 get-pip.py")
+# os.system("curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py")
+# os.system("python3 get-pip.py")
 
 # start frontend
 print("starting frontend...")
@@ -12,5 +12,5 @@ print("starting backend...")
 os.system("cd placecardAPI && npm i && npm start &")
 
 # # start server
-print("starting server")
-os.system("cd placecardAPI && pip install -r requirements.txt && python3 server.py &")
+# print("starting server")
+# os.system("cd placecardAPI && pip install -r requirements.txt && python3 server.py &")
