ccaaf89c4d7a8f3e26b80dc327bd133a984eb460
ksharo <ksharo@stevens.edu>
2022-05-31 12:34:00 -0400

    only heroku frontend

--- startScript.py
+++ startScript.py
@@ -5,11 +5,11 @@ import os
 
 # start frontend
 print("starting frontend...")
-os.system("cd placecardClient && npm i && npm start &")
+os.system("cd placecardClient && npm i && npm start")
 
 # # start backend
-print("starting backend...")
-os.system("cd placecardAPI && npm i && npm start &")
+# print("starting backend...")
+# os.system("cd placecardAPI && npm i && npm start &")
 
 # # start server
 # print("starting server")
